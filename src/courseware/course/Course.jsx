8d3be9bdc245c28cf323e088709501917a809063
Erarys <erarysabylkasym3@gmail.com>
2026-04-27 10:29:23 +0500

    add session sender

--- src/courseware/course/Course.jsx
+++ src/courseware/course/Course.jsx
@@ -78,13 +78,17 @@ const Course = ({
       console.log("HOOKED FINISH BUTTON ✅");
 
       btn.addEventListener("click", () => {
+        const params = new URLSearchParams(window.location.search);
+        const sessionId = params.get("session_id");
+
+        console.log("SESSION FROM URL:", sessionId);
         console.log("FINISH PROCTORING 🚀");
 
         const redirectUrl = window.location.href;
 
         // даём submitExam выполниться
         setTimeout(() => {
-          window.location.href = `https://open.kaznu.kz/finish-exam/?redirectUrl=${encodeURIComponent(redirectUrl)}`;
+          window.location.href = `https://open.kaznu.kz/finish-exam/?session_id=${sessionId}&redirectUrl=${encodeURIComponent(redirectUrl)}`;
         }, 1500);
       });
     }
